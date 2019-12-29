29b69c1d941310b447a9fc7e4f3cf578536f2780
Zeshan K <zeshan.khan@ymail.com>
2019-12-28 18:52:51 -0600

    Fix error with quotes.

--- writefile/writefile.sh
+++ writefile/writefile.sh
@@ -2,9 +2,8 @@
 
 if [ "$#" -gt 0 ]
 then
-    result=`echo "$2" | tr '$3' '\n'`
-    echo $result >> /home/pi/log
-    echo $result > $1
+    result=`echo "$2" | tr $3 '\n'`
+    echo "$result" | tee $1
     if [ "$4" = "Executable" ]
     then
         chmod +x $1
